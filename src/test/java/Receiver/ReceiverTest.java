28535f7fe23dab4214631faeffbfd7ef7e8d9231
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-04-22 19:29:00 +0530

    Update ReceiverTest.java

--- src/test/java/Receiver/ReceiverTest.java
+++ src/test/java/Receiver/ReceiverTest.java
@@ -13,14 +13,17 @@ import org.junit.Test;
 
 public class ReceiverTest {
 
-  List<Float> tempInputs = Arrays.asList(17f, 5f, 5f);
-  List<Float> socInputs = Arrays.asList(25f, 85f, 15f);
+  List<Float> tempInputs = Arrays.asList(17f, 5f, 5f, 68f, 89f);
+  List<Float> socInputs = Arrays.asList(25f, 85f, 15f, 3f, 56f);
 
-  List<Float> expectedMinTemp = Arrays.asList(17f, 5f, 5f);
-  List<Float> expectedMaxTemp = Arrays.asList(17f, 17f, 17f);
+  List<Float> expectedMinTemp = Arrays.asList(17f, 5f, 5f, 5f, 5f, 0.0f);
+  List<Float> expectedMaxTemp = Arrays.asList(17f, 17f, 17f, 68f, 89f, 100.0f);
 
-  List<Float> expectedMinSoc = Arrays.asList(25f, 25f, 15f);
-  List<Float> expectedMaxSoc = Arrays.asList(25f, 85f, 85f);
+  List<Float> expectedMinSoc = Arrays.asList(25f, 25f, 15f, 3f, 3f, 0.0f);
+  List<Float> expectedMaxSoc = Arrays.asList(25f, 85f, 85f, 85f, 85f, 99.0f);
+
+  List<Float> expectedSMATemp = Arrays.asList(0.0f, 0.0f, 0.0f, 0.0f, 36.8f, 22.2f);
+  List<Float> expectedSMASoc = Arrays.asList(0.0f, 0.0f, 0.0f, 0.0f, 36.8f, 39.4f);
 
   @Test
   public void testStatisticsInStream() {
@@ -30,11 +33,11 @@ public class ReceiverTest {
 
       assertEquals(this.expectedMinTemp.get(index), Receiver.minTempValue);
       assertEquals(this.expectedMaxTemp.get(index), Receiver.maxTempValue);
-      assertEquals(Float.valueOf(0.0f), Receiver.simpleMovingAvgForTemp);
+      assertEquals(this.expectedSMATemp.get(index), Receiver.simpleMovingAvgForTemp);
 
       assertEquals(this.expectedMinSoc.get(index), Receiver.minSocValue);
       assertEquals(this.expectedMaxSoc.get(index), Receiver.maxSocValue);
-      assertEquals(Float.valueOf(0.0f), Receiver.simpleMovingAvgForSoc);
+      assertEquals(this.expectedSMASoc.get(index), Receiver.simpleMovingAvgForSoc);
     }
   }
 
@@ -49,10 +52,18 @@ public class ReceiverTest {
           Receiver.findStatisticsForValuesInStream(inputsFromConsole[0], inputsFromConsole[1]);
           Receiver.printStatisticsToConsole();
         }
+        assertEquals(this.expectedMinTemp.get(5), Receiver.minTempValue);
+        assertEquals(this.expectedMaxTemp.get(5), Receiver.maxTempValue);
+        assertEquals(this.expectedSMATemp.get(5), Receiver.simpleMovingAvgForTemp);
+
+        assertEquals(this.expectedMinSoc.get(5), Receiver.minSocValue);
+        assertEquals(this.expectedMaxSoc.get(5), Receiver.maxSocValue);
+        assertEquals(this.expectedSMASoc.get(5), Receiver.simpleMovingAvgForSoc);
       }
     }
     catch (FileNotFoundException e) {
       e.printStackTrace();
     }
   }
+
 }
