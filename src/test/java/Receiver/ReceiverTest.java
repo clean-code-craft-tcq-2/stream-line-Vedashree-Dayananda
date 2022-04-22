d4db58d2a5b3859988b35344984b8a5fa4e457c5
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-04-22 11:36:08 +0530

    Update ReceiverTest.java

--- src/test/java/Receiver/ReceiverTest.java
+++ src/test/java/Receiver/ReceiverTest.java
@@ -1,10 +1,12 @@
 package Receiver;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import org.junit.Test;
 
@@ -16,25 +18,41 @@ public class ReceiverTest {
 
   List<Float> expectedMinTemp = Arrays.asList(17f, 5f, 5f);
   List<Float> expectedMaxTemp = Arrays.asList(17f, 17f, 17f);
-  List<Float> expectedSimpleMovingAvgTemp = Arrays.asList(Float.NaN, Float.NaN, Float.NaN);
 
   List<Float> expectedMinSoc = Arrays.asList(25f, 25f, 15f);
   List<Float> expectedMaxSoc = Arrays.asList(25f, 85f, 85f);
-  List<Float> expectedSimpleMovingAvgSoc = Arrays.asList();
 
   @Test
   public void testStatisticsInStream() {
-
+    Receiver.resetStatistics();
     for (int index = 0; index < this.tempInputs.size(); index++) {
       Receiver.findStatisticsForValuesInStream(this.tempInputs.get(index), this.socInputs.get(index));
 
       assertEquals(this.expectedMinTemp.get(index), Receiver.minTempValue);
       assertEquals(this.expectedMaxTemp.get(index), Receiver.maxTempValue);
-      assertTrue(Receiver.simpleMovingAvgForTemp.isNaN());
+      assertEquals(Float.valueOf(0.0f), Receiver.simpleMovingAvgForTemp);
 
       assertEquals(this.expectedMinSoc.get(index), Receiver.minSocValue);
       assertEquals(this.expectedMaxSoc.get(index), Receiver.maxSocValue);
-      assertTrue(Receiver.simpleMovingAvgForSoc.isNaN());
+      assertEquals(Float.valueOf(0.0f), Receiver.simpleMovingAvgForSoc);
+    }
+  }
+
+  @Test
+  public void testReceiver() {
+    File paramtersFile = new File("BatteryParameterData.txt");
+    Receiver.resetStatistics();
+    try {
+      try (Scanner inputScanner = new Scanner(paramtersFile)) {
+        for (int data = 0; data < Receiver.NO_OF_SENSOR_DATA; data++) {
+          Float[] inputsFromConsole = Receiver.getInputsFromConsole(inputScanner);
+          Receiver.findStatisticsForValuesInStream(inputsFromConsole[0], inputsFromConsole[1]);
+          Receiver.printStatisticsToConsole();
+        }
+      }
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
     }
   }
 }
